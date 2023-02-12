1f60434e083506e0a5469cb1e6d1fc413c23beed
Bartosz <nasieniu@gmail.com>
2023-02-12 17:26:37 +0100

    create menu for main weapon RAW

--- src/Eq/MainWeapon/mainWeapon.tsx
+++ src/Eq/MainWeapon/mainWeapon.tsx
@@ -1,7 +1,24 @@
+import { MainWeaponImageAndNameAndCost } from "../../data/equipment/mainWeapon";
+
 function MainWeapon() {
   return (
     <>
-      <div className="items-box MainWeapon"></div>
+      <div className="items-box MainWeapon">
+        <div id="option-container">
+          {MainWeaponImageAndNameAndCost.map((data, index) => {
+            return (
+              <div className="option" key={index}>
+                <img
+                  className="mainWeaponImg"
+                  src={data.image}
+                  alt={"Yuria weapon"}
+                />
+                <span className="itemName">{data.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/data/equipment/mainWeapon.tsx
@@ -0,0 +1,14 @@
+import image1 from "C:/Users/sieni/Desktop/ClickerZero/src/assets/images/mainWeapon/yuria.png";
+
+export const MainWeaponImageAndNameAndCost = [
+  {
+    image: image1,
+    name: "Yuria",
+    upgrade0: 150000,
+  },
+  {
+    image: image1,
+    name: "Grunil",
+    upgrade0: 350000,
+  },
+];
