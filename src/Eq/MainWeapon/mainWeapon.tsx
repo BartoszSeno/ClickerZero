90a1ab45195ad9c094181331018119cccafb70e8
Bartosz <nasieniu@gmail.com>
2023-02-12 19:20:44 +0100

    open and close menu for main weapon

--- src/Eq/MainWeapon/mainWeapon.tsx
+++ src/Eq/MainWeapon/mainWeapon.tsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import { MainWeaponImageAndNameAndCost } from "../../data/equipment/mainWeapon";
 
 function MainWeapon() {
+  //Geting id from loop on click
+  function GetIdPerClick(index: any) {
+    const item = MainWeaponImageAndNameAndCost[index];
+    console.log(item.id);
+  }
+
+  const [OpenAndClose, setOpenAndClose] = useState<boolean>(false);
+  function OpenClose() {
+    setOpenAndClose(!OpenAndClose);
+  }
   return (
     <>
-      <div className="items-box MainWeapon">
-        <div id="option-container">
+      <div className="items-box MainWeapon" onClick={() => OpenClose()}>
+        <div id="option-container" className={OpenAndClose ? "open" : "close"}>
           {MainWeaponImageAndNameAndCost.map((data, index) => {
             return (
-              <div className="option" key={index}>
+              <div
+                className={`option ${index} `}
+                key={index}
+                onClick={(e) => {
+                  GetIdPerClick(index);
+                }}
+              >
                 <img
                   className="mainWeaponImg"
                   src={data.image}
-                  alt={"Yuria weapon"}
+                  alt={`${data.name} weapon`}
                 />
                 <span className="itemName">{data.name}</span>
               </div>
--- src/data/equipment/mainWeapon.tsx
+++ src/data/equipment/mainWeapon.tsx
@@ -2,11 +2,13 @@ import image1 from "C:/Users/sieni/Desktop/ClickerZero/src/assets/images/mainWea
 
 export const MainWeaponImageAndNameAndCost = [
   {
+    id: 1,
     image: image1,
     name: "Yuria",
     upgrade0: 150000,
   },
   {
+    id: 2,
     image: image1,
     name: "Grunil",
     upgrade0: 350000,
