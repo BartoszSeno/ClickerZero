a59382eb69065dd874d6523642e29ef9c0bd77d7
Bartosz <nasieniu@gmail.com>
2023-03-04 09:07:25 +0100

    adding button arrow for close And Open left menu

--- src/App.tsx
+++ src/App.tsx
@@ -67,10 +67,75 @@ function App() {
     const scrollPosition = window.scrollY;
     leftContainer.style.backgroundPosition = `center ${scrollPosition}px`;
   });
+
+  // For open Left Menu
+  const [OpenMenu, setOpenMenu] = useState<boolean>(true);
+
+  function OpenMenuOrCloseMenu() {
+    setTimeout(() => {
+      const newValue = !OpenMenu;
+      setOpenMenu(newValue);
+      localStorage.setItem("OpenMenu", JSON.stringify(newValue));
+    }, 150);
+  }
+
+  // Retrieve the value of OpenMenu from local storage when the component mounts
+  useEffect(() => {
+    const valueFromLocalStorage = localStorage.getItem("OpenMenu");
+    if (valueFromLocalStorage !== null) {
+      setOpenMenu(JSON.parse(valueFromLocalStorage));
+    }
+  }, [OpenMenu]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setImgClick(
+        OpenMenu
+          ? "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Arrow/leftArrowButtonUnclicked.png"
+          : "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Arrow/rgihtArrowButtonUnclicked.png"
+      );
+    }, 300);
+  }, [OpenMenu]);
+
+  // image change script
+
+  const [ImgClick, setImgClick] = useState(
+    OpenMenu
+      ? "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Arrow/leftArrowButtonUnclicked.png"
+      : "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Arrow/rgihtArrowButtonUnclicked.png"
+  );
+
+  const changeImage = () => {
+    const originalSrc = ImgClick;
+    const newSrc = OpenMenu
+      ? "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Arrow/leftArrowButtonClicked.png"
+      : "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Arrow/rgihtArrowButtonClicked.png";
+
+    // Change the image source to the new image
+    setImgClick(newSrc);
+
+    // After 1 second, change the image source back to the original
+    setTimeout(() => {
+      setImgClick(originalSrc);
+    }, 100);
+  };
+
   return (
     <>
       <main id="App-container">
-        <div className="left-container">
+        <div
+          className="left-container"
+          style={OpenMenu ? { marginLeft: "0px" } : { marginLeft: "-635px" }}
+        >
+          <button
+            className="CloseAndOpenMenu"
+            onClick={(e) => {
+              OpenMenuOrCloseMenu();
+              changeImage();
+            }}
+          >
+            <span style={{ backgroundImage: `url(${ImgClick})` }}></span>
+          </button>
           <LeftNav setActiveTab={setActiveTab} activeTab={activeTab} />
 
           <div>
