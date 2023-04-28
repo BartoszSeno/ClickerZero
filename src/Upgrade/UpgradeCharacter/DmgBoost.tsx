4a632300cbcf45aac1626e78468952e61b0b58fd
BartoszSeno <bartoszymkowicz@gmail.com>
2023-04-28 21:20:08 +0200

    some fix

--- src/Upgrade/UpgradeCharacter/DmgBoost.tsx
+++ src/Upgrade/UpgradeCharacter/DmgBoost.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const DmgBoostt = ({
   savePontsForUpgrade,
   setsavePontsForUpgrade,
--- src/hook/hpBar.tsx
+++ src/hook/hpBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import "../assets/css/Normal/hpBar/hpbar.css";
 
