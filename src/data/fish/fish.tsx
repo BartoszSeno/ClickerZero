d53c34318f17fb676d90b63a5d513b74d46c8a92
BartoszSeno <bartoszymkowicz@gmail.com>
2023-05-09 22:38:29 +0200

    adding fish img and fish data

---
+++ src/data/fish/fish.tsx
@@ -0,0 +1,82 @@
+export const FishelectionStart = [
+  {
+    id: 1,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Muskellunge.png",
+    name: "Muskellunge",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 2,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Ernpike.png",
+    name: "Ernpike",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 3,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Laketrout.png",
+    name: "Laketrout",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 4,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Crayfish.png",
+    name: "Cray",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 5,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/catFish.png",
+    name: "Cat Fish",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 6,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Crappie.png",
+    name: "Crappie",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 7,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Turgeon.png",
+    name: "Turgeon",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 8,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Bluepike.png",
+    name: "Bluepike",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 9,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Sauger.png",
+    name: "Sauger",
+    isUnlocked: false,
+    type: "Fish",
+  },
+  {
+    id: 10,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Fish/Sunfish.png",
+    name: "Sunfish",
+    isUnlocked: false,
+    type: "Fish",
+  },
+];
