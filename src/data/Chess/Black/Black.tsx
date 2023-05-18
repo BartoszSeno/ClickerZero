f5a2411ec824ca8d9a5b2c9bc16d8c2640b29a4a
BartoszSeno <bartoszymkowicz@gmail.com>
2023-05-18 20:37:26 +0200

    added data for Black and White chess pice

---
+++ src/data/Chess/Black/Black.tsx
@@ -0,0 +1,44 @@
+export const ChessBlack = [
+  {
+    id: 1,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/Black/Pawn.png",
+    name: "Pawn",
+    color: "Black",
+  },
+  {
+    id: 2,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/Black/RookBlack.png",
+    name: "Rook",
+    color: "Black",
+  },
+  {
+    id: 3,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/Black/KnightBlack.png",
+    name: "Knight",
+    color: "Black",
+  },
+  {
+    id: 4,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/Black/BishopBlack.png",
+    name: "Bishop",
+    color: "Black",
+  },
+  {
+    id: 5,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/Black/QueenBlack.png",
+    name: "Queen",
+    color: "Black",
+  },
+  {
+    id: 6,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/Black/KingBlack.png",
+    name: "King",
+    color: "Black",
+  },
+];
---
+++ src/data/Chess/White/White.tsx
@@ -0,0 +1,44 @@
+export const ChessWhite = [
+  {
+    id: 1,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/White/PawnWhite.png",
+    name: "Pawn",
+    color: "White",
+  },
+  {
+    id: 2,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/White/RookWhite.png",
+    name: "Rook",
+    color: "White",
+  },
+  {
+    id: 3,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/White/KnightWhite.png",
+    name: "Knight",
+    color: "White",
+  },
+  {
+    id: 4,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/White/BishopWhite.png",
+    name: "Bishop",
+    color: "White",
+  },
+  {
+    id: 5,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/White/QueenWhite.png",
+    name: "Queen",
+    color: "White",
+  },
+  {
+    id: 6,
+    image:
+      "https://raw.githubusercontent.com/BartoszSeno/ClickerZero/main/src/assets/MainImg/Chess/White/KingWhite.png",
+    name: "King",
+    color: "White",
+  },
+];
