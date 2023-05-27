21051017e73e98da35e86a44d6b3d064a8226251
BartoszSeno <bartoszymkowicz@gmail.com>
2023-05-27 17:50:27 +0200

    added button next round that gived player one random card and on every 3 round unlock strongerst card

---
+++ src/hook/CardGame/HandCard/HandAlly.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import { AllyCard } from "../../../data/Card/Ally";
+
+function HandAlly({ randomItems }: { randomItems: any }) {
+  //console.log(remainingItems);
+  return (
+    <>
+      <div className="AllyHandCard">
+        <div className="scaleSize">
+          {randomItems.map((data: any, index: any) => (
+            <div className="CardConteiner" key={index}>
+              <div className="imgCardAb"></div>
+              <div className="ManaC">
+                <div className="ManaPoints">{data.Mana}</div>
+              </div>
+              <div
+                className="CardChar"
+                style={{
+                  backgroundImage: `url(${data.img})`,
+                  backgroundPositionY:
+                    data.id === 1
+                      ? "10px"
+                      : data.id === 2
+                      ? "30px"
+                      : data.id === 3
+                      ? "35px"
+                      : data.id === 4
+                      ? "30px"
+                      : data.id === 5
+                      ? "55px"
+                      : data.id === 6
+                      ? "-40px"
+                      : data.id === 7
+                      ? "50px"
+                      : data.id === 8
+                      ? "70px"
+                      : data.id === 9
+                      ? "67px"
+                      : data.id === 10
+                      ? "70px"
+                      : data.id === 11
+                      ? "55px"
+                      : data.id === 12
+                      ? "40px"
+                      : data.id === 13
+                      ? "65px"
+                      : data.id === 14
+                      ? "50px"
+                      : data.id === 15
+                      ? "30px"
+                      : data.id === 16
+                      ? "25px"
+                      : data.id === 17
+                      ? "50px"
+                      : data.id === 18
+                      ? "15px"
+                      : data.id === 19
+                      ? "50px"
+                      : data.id === 20
+                      ? "65px"
+                      : data.id === 21
+                      ? "62px"
+                      : data.id === 22
+                      ? "30px"
+                      : "",
+                  backgroundSize:
+                    data.id === 1
+                      ? "200px"
+                      : data.id === 2
+                      ? "200px"
+                      : data.id === 3
+                      ? "200px"
+                      : data.id === 5
+                      ? "230px"
+                      : data.id === 6
+                      ? "250px"
+                      : data.id === 8
+                      ? "200px"
+                      : data.id === 10
+                      ? "100px"
+                      : data.id === 12
+                      ? "200px"
+                      : data.id === 14
+                      ? "200px"
+                      : data.id === 15
+                      ? "260px"
+                      : data.id === 16
+                      ? "150px"
+                      : data.id === 17
+                      ? "120px"
+                      : data.id === 18
+                      ? "250px"
+                      : data.id === 19
+                      ? "220px"
+                      : "",
+                  backgroundPositionX:
+                    data.id === 1
+                      ? "25px"
+                      : data.id === 15
+                      ? "-20px"
+                      : data.id === 20
+                      ? "25px"
+                      : "",
+                }}
+              ></div>
+              <div className="CardName">{data.Name}</div>
+              <div className="CardStats">
+                <div className="AtackPoints">{data.Atack}</div>
+                <div className="HpPoints">{data.Hp}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HandAlly;
--- src/hook/CardGame/index.tsx
+++ src/hook/CardGame/index.tsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../../assets/css/Normal/CardGame/CardGame.css";
+import HandAlly from "./HandCard/HandAlly";
+import HandEnemy from "./HandCard/HandEnemy";
+import { EnemyCard } from "../../data/Card/Enemy";
+import { AllyCard } from "../../data/Card/Ally";
 
 function CardGame({
   OpenCardGame,
@@ -13,7 +17,169 @@ function CardGame({
   function CloseCardGame() {
     setOpenCardGame(false);
   }
+  //=====================
+  const [randomItemsE, setrandomItemsE] = useState<any>([]);
+  const [remainingItemsE, setremainingItemsE] = useState<number[]>([]);
+  const [clickCountE, setclickCountE] = useState<number>(0);
 
+  useEffect(() => {
+    const initialIndexes = Array.from({ length: 7 }, (_, index) => index);
+    setremainingItemsE(initialIndexes);
+  }, []);
+
+  useEffect(() => {
+    if (clickCountE === 3) {
+      setremainingItemsE((prevremainingItemsE) => {
+        const additionalIndexes = [7, 8, 9];
+        return [...prevremainingItemsE, ...additionalIndexes];
+      });
+    }
+    if (clickCountE === 6) {
+      setremainingItemsE((prevremainingItemsE) => {
+        const additionalIndexes = [10, 11, 12];
+        return [...prevremainingItemsE, ...additionalIndexes];
+      });
+    }
+    if (clickCountE === 9) {
+      setremainingItemsE((prevremainingItemsE) => {
+        const additionalIndexes = [13, 14, 15];
+        return [...prevremainingItemsE, ...additionalIndexes];
+      });
+    }
+    if (clickCountE === 12) {
+      setremainingItemsE((prevremainingItemsE) => {
+        const additionalIndexes = [16, 17, 18];
+        return [...prevremainingItemsE, ...additionalIndexes];
+      });
+    }
+    if (clickCountE === 15) {
+      setremainingItemsE((prevremainingItemsE) => {
+        const additionalIndexes = [19, 20, 21];
+        return [...prevremainingItemsE, ...additionalIndexes];
+      });
+    }
+  }, [clickCountE]);
+
+  // Sprawdzenie, czy przedmiot już został wylosowany
+  const isItemAlreadySelectedE = (item: any): boolean => {
+    return randomItemsE.includes(item);
+  };
+
+  // Losowanie przedmiotu bez powtórzeń z ograniczonym zakresem
+  const getRandomItemE = (): any | undefined => {
+    const availableIndexes = remainingItemsE.filter(
+      (index) => !isItemAlreadySelectedE(EnemyCard[index])
+    );
+    if (availableIndexes.length === 0) {
+      console.log("Brak dostępnych przedmiotów.");
+      return undefined;
+    }
+    const randomIndex = Math.floor(Math.random() * availableIndexes.length);
+    const index = availableIndexes[randomIndex];
+    return EnemyCard[index];
+  };
+
+  //=====================
+  const [randomItems, setRandomItems] = useState<any>([]);
+  const [remainingItems, setRemainingItems] = useState<number[]>([]);
+  const [clickCount, setClickCount] = useState<number>(0);
+
+  useEffect(() => {
+    const allyCardItems = AllyCard;
+    const initialIndexes = Array.from({ length: 7 }, (_, index) => index);
+    setRemainingItems(initialIndexes);
+  }, []);
+
+  useEffect(() => {
+    if (clickCount === 3) {
+      setRemainingItems((prevRemainingItems) => {
+        const additionalIndexes = [7, 8, 9];
+        return [...prevRemainingItems, ...additionalIndexes];
+      });
+    }
+    if (clickCount === 6) {
+      setRemainingItems((prevRemainingItems) => {
+        const additionalIndexes = [10, 11, 12];
+        return [...prevRemainingItems, ...additionalIndexes];
+      });
+    }
+    if (clickCount === 9) {
+      setRemainingItems((prevRemainingItems) => {
+        const additionalIndexes = [13, 14, 15];
+        return [...prevRemainingItems, ...additionalIndexes];
+      });
+    }
+    if (clickCount === 12) {
+      setRemainingItems((prevRemainingItems) => {
+        const additionalIndexes = [16, 17, 18];
+        return [...prevRemainingItems, ...additionalIndexes];
+      });
+    }
+    if (clickCount === 15) {
+      setRemainingItems((prevRemainingItems) => {
+        const additionalIndexes = [19, 20, 21];
+        return [...prevRemainingItems, ...additionalIndexes];
+      });
+    }
+  }, [clickCount]);
+
+  // Sprawdzenie, czy przedmiot już został wylosowany
+  const isItemAlreadySelected = (item: any): boolean => {
+    return randomItems.includes(item);
+  };
+
+  // Losowanie przedmiotu bez powtórzeń z ograniczonym zakresem
+  const getRandomItem = (): any | undefined => {
+    const availableIndexes = remainingItems.filter(
+      (index) => !isItemAlreadySelected(AllyCard[index])
+    );
+    if (availableIndexes.length === 0) {
+      console.log("Brak dostępnych przedmiotów.");
+      return undefined;
+    }
+    const randomIndex = Math.floor(Math.random() * availableIndexes.length);
+    const index = availableIndexes[randomIndex];
+    return AllyCard[index];
+  };
+
+  const addRandomItemWithoutRepetition = () => {
+    const newItemE = getRandomItemE();
+    if (newItemE) {
+      setrandomItemsE((prevItems: any) => [...prevItems, newItemE]);
+      setremainingItemsE((prevIndexes) => {
+        const newIndexes = [...prevIndexes];
+        const itemIndex = EnemyCard.findIndex((item) => item === newItemE);
+        if (itemIndex !== -1) {
+          const remainingIndex = newIndexes.findIndex(
+            (index) => index === itemIndex
+          );
+          if (remainingIndex !== -1) {
+            newIndexes.splice(remainingIndex, 1);
+          }
+        }
+        return newIndexes;
+      });
+      setclickCountE((prevCount) => prevCount + 1);
+    }
+    const newItem = getRandomItem();
+    if (newItem) {
+      setRandomItems((prevItems: any) => [...prevItems, newItem]);
+      setRemainingItems((prevIndexes) => {
+        const newIndexes = [...prevIndexes];
+        const itemIndex = AllyCard.findIndex((item) => item === newItem);
+        if (itemIndex !== -1) {
+          const remainingIndex = newIndexes.findIndex(
+            (index) => index === itemIndex
+          );
+          if (remainingIndex !== -1) {
+            newIndexes.splice(remainingIndex, 1);
+          }
+        }
+        return newIndexes;
+      });
+      setClickCount((prevCount) => prevCount + 1);
+    }
+  };
   return (
     <>
       <div
@@ -32,7 +198,22 @@ function CardGame({
             setCardGameIsOpen(true);
           }}
         >
-          <></>
+          <div className="CloseGameP">
+            <button
+              className="CloseGame"
+              onClick={(e) => {
+                e.stopPropagation();
+                CloseCardGame();
+              }}
+            >
+              X
+            </button>
+          </div>
+          <div className="GameCP">
+            <HandEnemy randomItemsE={randomItemsE} />
+            <button onClick={addRandomItemWithoutRepetition}>Next Round</button>
+            <HandAlly randomItems={randomItems} />
+          </div>
         </div>
       </div>
     </>
